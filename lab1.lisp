cdeb468482a9a6ca1dcb0efddb4522cd0c518d17
VintaChlenix <VintaChlenix@users.noreply.github.com>
2022-10-10 09:34:44 +0300

    .

--- lab1.lisp
+++
@@ -1,4 +0,0 @@
-(defun queen-moves (k l m n)
-  (if (or (= m k) (= n l) (= (abs (- m k)) (abs (- n l)))) T
-       (values m l)))
-    
--- lab2.lisp
+++
@@ -1,5 +0,0 @@
-(defun nth-element (i lst)
-  (if (> i 0) 
-      (nth-element (- i 1) (cdr lst))
-      (first lst)))
-                           
